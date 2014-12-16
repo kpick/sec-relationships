352ad9d8b3fc887b57b2efa446828ac165ca1276
Ken <kpick@live.com>
2014-12-16 14:12:38 -0500

    Fixing new issues with Express... Currently unstable.

--- app.js
+++ app.js
@@ -13,23 +13,23 @@ var app = express();
 app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.logger('dev'));
-app.use(express.bodyParser());
-app.use(express.methodOverride());
-app.use(app.router);
+//app.use(express.favicon());
+//app.use(express.logger('dev'));
+//app.use(express.bodyParser());
+//app.use(express.methodOverride());
+//app.use(app.router);
 
 // development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
+//if ('development' == app.get('env')) {
+//  app.use(express.errorHandler());
+//}
 
-app.locals({
-    title: 'Define Relationship'    // default title
-});
+//app.locals({
+//    title: 'Define Relationship'    // default title
+	//});
 
-// Routes
+//Routes
 
 app.get('/', routes.site.index);
 
